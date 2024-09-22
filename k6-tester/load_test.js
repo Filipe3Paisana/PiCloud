96d1e11c630f57c190d3335ac6bc26be3f857611
Filipe Paisana <20220843@iade.pt>
2024-09-22 19:51:04 +0100

    Container de testes a funcionar

---
+++ k6-tester/load_test.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6'; // Adiciona a importação de 'check'
+
+export let options = {
+    stages: [
+        
+        { duration: '1m', target: 5000 },  // Aumenta até 5000 usuários em 1 minuto
+        
+    ],
+};
+
+export default function () {
+  const res = http.get('http://nginx-container:8080');
+  
+  // Verifica se a resposta tem o status 200
+  check(res, { 'status was 200': (r) => r.status == 200 });
+
+  sleep(1); // Dorme por 1 segundo entre as requisições
+}
