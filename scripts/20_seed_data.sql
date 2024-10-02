1cbf5877e95a126cc37c843509cbcaed144fabfd
Manuel Silva <mjgrs2002@gmail.com>
2024-10-02 23:40:28 +0100

    tabelas da base de dados criadas e inserts a funcionar

--- scripts/20_seed_data.sql
+++ scripts/20_seed_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO "User" (name, email, password) 
+INSERT INTO Users (username, email, password_hash) 
 VALUES 
 ('Alice', 'alice@example.com', 'password123'),
 ('Bob', 'bob@example.com', 'securePass!'),
