9904bb7ba5c1620a06d38270b298aa110059a361
Filipe Paisana <20220843@iade.pt>
2024-10-02 23:12:54 +0100

    .env a funcionar

---
+++ scripts/20_seed_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO "User" (name, email, password) 
+VALUES 
+('Alice', 'alice@example.com', 'password123'),
+('Bob', 'bob@example.com', 'securePass!'),
+('Charlie', 'charlie@example.com', 'charlie123');
