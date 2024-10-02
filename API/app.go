5b4894ea326a7e8a1181e73141057e48db5106aa
Filipe Paisana <20220843@iade.pt>
2024-10-02 15:19:22 +0100

    Server de GO a rodar

---
+++ API/app.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+func main() {
+	
+	fmt.Println("Starting server on :8081")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
---
+++ db-init-scripts/db-init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE user (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (username, email, password_hash) VALUES
+('john_doe', 'john@example.com', 'hashed_password_1'),
+('jane_smith', 'jane@example.com', 'hashed_password_2'),
+('alice_jones', 'alice@example.com', 'hashed_password_3');
