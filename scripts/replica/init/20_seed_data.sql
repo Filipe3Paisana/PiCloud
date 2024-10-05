2d0adaa181513f2b3cff0d673c4995f771d5c545
Filipe Paisana <20220843@iade.pt>
2024-10-05 01:49:53 +0100

    Replica de DB feita com sucesso

---
+++ scripts/replica/init/20_seed_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Users (username, email, password_hash) 
+VALUES 
+('Alice', 'alice@example.com', 'password123'),
+('Bob', 'bob@example.com', 'securePass!'),
+('Charlie', 'charlie@example.com', 'charlie123');
