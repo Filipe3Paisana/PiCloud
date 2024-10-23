44159d6ce32a6cef104db3d1b915aed796a53034
Filipe Paisana <20220843@iade.pt>
2024-10-23 20:26:45 +0100

    Next staps to download

---
+++ API/handlers/download.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"api/db"
+	"api/models"
+	"api/utils"
+)
+
+
+
+func DownloadHandler() {
+	reciveFragment()
+
+	return nil
+}
+
+
+func reciveFragment() {
+	// Função identica ao distribuitFragment
+	getFragmentFromNode()
+	return nil
+}
+
+func getFragmentFromNode() {
+	return nil
+}
+
+func sendFile()	{
+	return nil
+}
+ 
