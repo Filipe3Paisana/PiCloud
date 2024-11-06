58acf3600b621fb8342f65e836d1a5e875a3f8bb
Filipe Paisana <20220843@iade.pt>
2024-11-06 18:22:26 +0000

    Configurações na base de dados e adicionar comando

--- DB/doc/int_replication.sh
+++ DB/doc/int_replication.sh
@@ -1,7 +1,8 @@
-#createuser -U test -P -c 5 --replication reptest
-#pg_basebackup -h postgres-container -p 5432 -U reptest -D /var/lib/postgresql/data -Fp -Xs -R -P
-
-
+createuser -U test -P -c 5 --replication reptest
+pg_basebackup -h postgres-container -p 5432 -U reptest -D /data/ -Fp -Xs -R -P
+pg_basebackup -h postgres-1 -p 5432 -U reptest -D /var/lib/postgresql/data -Fp -Xs -R -P
+psql -U test -c "SELECT * FROM pg_stat_replication;"
+primary_conninfo = 'host=postgres-container port=5432 user=reptest password=test'
 # login to postgres
 psql --username=test test
 
